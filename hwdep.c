abc917285e5df50b767712914702a08eb64f9a81
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-03-15 22:10:17 +0900

    hwdep: add ioctl test program for ALSA hwdep interface

---
+++ hwdep.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <sys/ioctl.h>
+
+#include <errno.h>
+#include <string.h>
+
+#include <sound/asound.h>
+
+#define TEST_IOCTL(fd, command, argument, expected)			\
+	if (ioctl(fd, command, argument) < 0 && errno != expected) {	\
+		printf("%s: %s\n", #command, strerror(errno));		\
+		return false;						\
+	}								\
+	return true;							\
+
+static bool check_pversion(int fd)
+{
+	int version;
+	TEST_IOCTL(fd, SNDRV_HWDEP_IOCTL_PVERSION, &version, 0);
+}
+
+static bool check_info(int fd)
+{
+	struct snd_hwdep_info info = {0};
+	TEST_IOCTL(fd, SNDRV_HWDEP_IOCTL_INFO, &info, 0);
+}
+
+int main(void)
+{
+	bool (*const funcs[])(int) = {
+		check_pversion,
+		check_info,
+		NULL,
+	};
+	unsigned int i;
+	int fd;
+
+	fd = open("/dev/snd/hwC0D0", O_RDONLY);
+	if (fd < 0) {
+		printf("%s\n", strerror(errno));
+		return EXIT_FAILURE;
+	}
+
+	i = 0;
+	while (funcs[i]) {
+		if (!funcs[i++](fd)) {
+			printf("Hwdep test aborts.\n");
+			return EXIT_FAILURE;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
